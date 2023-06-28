4cd413875d0f37d5fa75673af5cd3a0688f5535e
Filip Lizoń <filip.lizon1@gmail.com>
2023-06-28 18:58:04 +0200

    refactor: services section

--- js/services.js
+++ js/services.js
@@ -1,20 +1,20 @@
-const seeMoreBtn = document.querySelectorAll('.services__btn');
+const seeMoreBtns = document.querySelectorAll(".services__btn");
 
-const btnDown = (e) => {
-    e.target.classList.toggle('services__btn--down');
-    if (e.target.classList.contains('services__btn--down')) {
-        e.target.textContent = 'Inhoud verbergen'
-    } else {
-        e.target.textContent = 'Bekijk meer'
-    }
-}
+const btnDown = btn => {
+  btn.classList.toggle("services__btn--down");
+  btn.textContent = btn.classList.contains("services__btn--down")
+    ? "Inhoud verbergen"
+    : "Bekijk meer";
+};
 
-const showText = (e) => {
-    btnDown(e);
-    e.target.parentNode.firstChild.nextSibling.classList.toggle('services__title--up');
-    e.target.parentNode.firstChild.nextSibling.nextSibling.nextSibling.classList.toggle('services__text--appear');
-}
+const showText = btn => {
+  btnDown(btn);
+  const title = btn.parentNode.querySelector(".services__title");
+  const text = btn.parentNode.querySelector(".services__text");
+  title.classList.toggle("services__title--up");
+  text.classList.toggle("services__text--appear");
+};
 
-seeMoreBtn.forEach(btn => {
-    btn.addEventListener('click', showText);
-})
+seeMoreBtns.forEach(btn => {
+  btn.addEventListener("click", () => showText(btn));
+});
