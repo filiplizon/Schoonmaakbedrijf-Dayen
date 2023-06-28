6ad00a4e1fc65b20edf98059b1239e037849ded1
Filip Lizoń <filip.lizon1@gmail.com>
2023-06-28 22:35:43 +0200

    refactor js

--- js/animations.js
+++ js/animations.js
@@ -1,44 +1,43 @@
-const aboutHeading = document.querySelector('.about__heading');
-const aboutText = document.querySelector('.about__text');
-const aboutPhoto = document.querySelector('.about__photo')
-const servicesHeading = document.querySelector('.services__heading');
-const servicesText = document.querySelector('.services__describtion');
-const about = document.querySelector('.about');
-const services = document.querySelector('.services');
-const features = document.querySelector('.features');
-const featuresHeadings = document.querySelectorAll('.features__title');
-const featuresTexts = document.querySelectorAll('.features__text');
+const about = document.querySelector(".about");
+const services = document.querySelector(".services");
+const features = document.querySelector(".features");
+const aboutHeading = about.querySelector(".about__heading");
+const aboutText = about.querySelector(".about__text");
+const aboutPhoto = about.querySelector(".about__photo");
+const servicesHeading = services.querySelector(".services__heading");
+const servicesText = services.querySelector(".services__describtion");
+const featuresHeadings = features.querySelectorAll(".features__title");
+const featuresTexts = features.querySelectorAll(".features__text");
 
 function isAnyPartOfElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
-    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
-    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+  const { top, height, left, width } = el.getBoundingClientRect();
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+  const vertInView = top <= windowHeight && top + height >= 0;
+  const horInView = left <= windowWidth && left + width >= 0;
 
-    return (vertInView && horInView);
+  return vertInView && horInView;
 }
 
-const addClassAfterScroll = () => {
-    if (isAnyPartOfElementInViewport(about)) {
-        aboutHeading.classList.add('about__heading--slide');
-        aboutText.classList.add('about__text--slide');
-        aboutPhoto.classList.add('about__photo--active');
+window.addEventListener("scroll", () => {
+  if (isAnyPartOfElementInViewport(about)) {
+    aboutHeading.classList.add("about__heading--slide");
+    aboutText.classList.add("about__text--slide");
+    aboutPhoto.classList.add("about__photo--active");
+  }
+  if (isAnyPartOfElementInViewport(services)) {
+    servicesHeading.classList.add("services__heading--appear");
+    servicesText.classList.add("services__describtion--appear");
+  }
+  featuresHeadings.forEach(heading => {
+    if (isAnyPartOfElementInViewport(heading)) {
+      heading.classList.add("features__title--slide-in-from-right");
     }
-    if (isAnyPartOfElementInViewport(services)) {
-        servicesHeading.classList.add('services__heading--appear');
-        servicesText.classList.add('services__describtion--appear');
+  });
+  featuresTexts.forEach(text => {
+    if (isAnyPartOfElementInViewport(text)) {
+      text.classList.add("features__text--slide-in-from-right");
     }
-    featuresHeadings.forEach((heading) => {
-        if (isAnyPartOfElementInViewport(heading)) {
-            heading.classList.add('features__title--slide-in-from-right');
-        }
-    })
-    featuresTexts.forEach((text) => {
-        if (isAnyPartOfElementInViewport(text)) {
-            text.classList.add('features__text--slide-in-from-right');
-        }
-    })
-}
-
-window.addEventListener('scroll', addClassAfterScroll);
+  });
+});
--- js/navigation.js
+++ js/navigation.js
@@ -3,11 +3,15 @@ const nav = document.querySelector(".nav");
 const navigationList = document.querySelector(".nav__list");
 const navLogo = document.querySelector(".nav__logo");
 const navLink = document.querySelectorAll(".nav__link");
-let viewportWidth = window.innerWidth;
+const viewportWidth = window.innerWidth;
 
-const changeNavigationVisibility = () => {
+const toggleNavigation = () => {
   menuButton.classList.toggle("nav__menu--is-open");
   navigationList.classList.toggle("nav__list--is-open");
+  toggleBodyOverflow();
+};
+
+const toggleBodyOverflow = () => {
   if (
     navigationList.classList.contains("nav__list--is-open") &&
     viewportWidth < 1024
@@ -20,18 +24,14 @@ const changeNavigationVisibility = () => {
 
 const modifyNav = () => {
   if (viewportWidth > 1024) {
-    if (window.scrollY > 100) {
-      nav.classList.add("nav--after-scroll");
-      navLogo.classList.add("nav__logo--small");
-    } else {
-      nav.classList.remove("nav--after-scroll");
-      navLogo.classList.remove("nav__logo--small");
-    }
+    const isScrolled = window.scrollY > 100;
+    nav.classList.toggle("nav--after-scroll", isScrolled);
+    navLogo.classList.toggle("nav__logo--small", isScrolled);
   }
 };
 
 window.addEventListener("scroll", modifyNav);
-menuButton.addEventListener("click", changeNavigationVisibility);
+menuButton.addEventListener("click", toggleNavigation);
 navLink.forEach(link => {
-  link.addEventListener("click", changeNavigationVisibility);
+  link.addEventListener("click", toggleNavigation);
 });
