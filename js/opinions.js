ca09c6f4741210fca3393cdc0139646725753d7d
Filip Lizoń <filip.lizon1@gmail.com>
2023-06-28 22:22:37 +0200

    refactor: opinions

--- js/opinions.js
+++ js/opinions.js
@@ -1,47 +1,50 @@
-const opinion = document.querySelector('.opinions__opinion');
-const author = document.querySelector('.opinions__author-name');
-const companyName = document.querySelector('.opinions__author-company');
-const arrows = document.querySelectorAll('.opinions__arrow');
-const currentOpinion = document.getElementById('current-opinion');
-const opinionsLength = document.getElementById('opinions-length');
+const container = document.querySelector(".opinions");
+const [opinion, author, companyName] = container.querySelectorAll(
+  ".opinions__opinion, .opinions__author-name, .opinions__author-company"
+);
+const arrows = container.querySelectorAll(".opinions__arrow");
+const currentOpinion = document.getElementById("current-opinion");
+const opinionsLength = document.getElementById("opinions-length");
 
-let opinions = ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vel quia explicabo.', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum in suscipit voluptatibus autem. Facere, dignissimos corporis. Temporibus.', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'];
-let authors = ['Adrie Lans', 'Andre van Laarhoven', 'Beata Bronkchorst']
-let companies = ['Palletbedrijf Lans', 'Laarhoven Pallets', 'Restaurant Hilton']
+const opinions = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vel quia explicabo.",
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum in suscipit voluptatibus autem. Facere, dignissimos corporis. Temporibus.",
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+];
+const authors = ["Adrie Lans", "Andre van Laarhoven", "Beata Bronkchorst"];
+const companies = [
+  "Palletbedrijf Lans",
+  "Laarhoven Pallets",
+  "Restaurant Hilton",
+];
 
 let index = 0;
-let opinionNumber = 1;
 
 function showOpinion() {
-    opinion.textContent = opinions[index];
-    author.textContent = authors[index];
-    companyName.textContent = companies[index];
-    opinionsLength.textContent = opinions.length;
-    currentOpinion.textContent = opinionNumber;
+  opinion.textContent = opinions[index];
+  author.textContent = authors[index];
+  companyName.textContent = companies[index];
+  opinionsLength.textContent = opinions.length;
+  currentOpinion.textContent = index + 1;
 }
 
 showOpinion();
 
-const changeOpinion = (e) => {
-    let changeDirection = e.target.dataset.direction;
-    if (changeDirection == 'right') {
-        index++;
-        opinionNumber++
-        if (index === opinions.length) {
-            index = 0;
-            opinionNumber = 1;
-        }
+const changeOpinion = e => {
+  let changeDirection = e.target.dataset.direction;
+  if (changeDirection == "right") {
+    index++;
+    if (index === opinions.length) {
+      index = 0;
     }
-    if (changeDirection == 'left') {
-        console.log('działa');
-        index--;
-        opinionNumber--;
-        if (index < 0) {
-            index = opinions.length - 1;
-            opinionNumber = opinions.length;
-        }
+  }
+  if (changeDirection == "left") {
+    index--;
+    if (index < 0) {
+      index = opinions.length - 1;
     }
-    showOpinion();
-}
+  }
+  showOpinion();
+};
 
-arrows.forEach(arrow => arrow.addEventListener('click', changeOpinion));
+arrows.forEach(arrow => arrow.addEventListener("click", changeOpinion));
